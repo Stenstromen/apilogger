374978402b241562a655abe41fe49f91a776332d
Stenstromen <peppoji@gmail.com>
2022-05-26 15:49:29 +0200

    Removed router dependency for simplicity

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 const express = require('express');
-const router = express.Router();
 const bodyParser = require('body-parser');
 const app = express();
 const port = 8080;
-app.use("/", router);
 
-//body-parser middleware
-app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+let urlencodedParser = bodyParser.urlencoded({ extended: false })  
 
-router.post('/api/log/info', (req, res) => {
-    console.log(req.body);
-    res.end();
-})
+app.post('/api/test', urlencodedParser, function (req, res) {  
+    response = {  
+        name:req.body.name
+    };  
+    console.log(response.name);  
+    res.end(JSON.stringify(response));  
+ })
+ 
+ app.post('/api/test2', urlencodedParser, function (req, res) { 
+    response = {  
+        animal:req.body.animal
+    };  
+    console.log(response.animal);  
+    res.end(JSON.stringify(response));  
+ })  
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
